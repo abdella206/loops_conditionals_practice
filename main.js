3a5f507a82ca77ac91154cf27b913886011eedcf
abdella206 <f.abdella1@yahoo.com>
2019-03-21 01:47:29 -0700

    my answers

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// Answer for # 1
+/*
+for (var i =0; i <= 100; i++ )
+console.log(i)
+*/
+
+
+// Answer for # 2
+/*
+for (var i =7; i <= 635; i++ )
+console.log(i)
+*/
+
+// Answer for # 3
+/*
+var start = 0;
+var limit = 100;
+for (var i= start; i <= limit; i++)
+console.log(i);
+*/
+
+// Answer for # 4
+/*
+var name = ["F", "u", "a", "d"];
+
+for (var i = 0; i < name.length; i += 1){
+    console.log(name[i]);
+}
+*/
